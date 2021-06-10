a0afa8f09227957e4c0999ac0b348bc583bf3a48
Lawal Hakeem <lawizyhal@gmail.com>
2021-06-10 10:06:17 +0200

    minor commit

--- models/studentSchema.js
+++ models/studentSchema.js
@@ -1,48 +1,55 @@
 const mongoose=require("mongoose")
+const validator=require("validator")
 const studentSchema=mongoose.Schema({
 
     studNum:{
         type:Number,
-        required:true
+        required:[true, "please provide your matric Number"]
     },
 
     studFname:{
         type:String,
-        required:true
+        required:[true, "please provide your first name"]
     },
 
     studMname:{
         type:String,
-        required:true
+        required:[true, "please provide your middle name"]
     },
     studLname:{
         type:String,
-        required:true
+        required:[true, "please provide your last name"]
     },
 
     studAddressReg:{
         type:String,
-        required:true
+        required:[true, "please provide your registered address"]
     },
 
     studPhone:{
         type:String,
-        required:true
+        required:[true, "please provide your phone number"]
     },
 
     studGender:{
         type:String,
-        required:true
+        required:[true, "please provide your gender"],
+        enum:["male","female"]
     },
 
     studAge:{
         type:String,
-        required:true
+        required:[true, "please provide your age"]
     },
 
     studEmailAddress:{
         type:String,
-        required:true
+        required:[true, "please provide your email"],
+        unique:true,
+        lowecase:true,
+        validate:[validator.isEmail, "Please provide a valid email"]
+
+
     },
 
     YearOfEnrollment:{
--- models/teacherSchema.js
+++ models/teacherSchema.js
@@ -1,50 +1,51 @@
 const mongoose=require("mongoose")
+const validator=require("validator")
 const teacherSchema=mongoose.Schema({
 
     admin:Boolean,
 
-    teacherNum:{
-        type:Number,
-        required:true
-    },
-
     teacherFname:{
         type:String,
-        required:true
+        required:[true, "please provide your first name"]
+    
     },
 
     teacherMname:{
         type:String,
-        required:true
+        required:[true, "please provide your middle name"]
     },
     teacherLname:{
         type:String,
-        required:true
+        required:[true, "please provide your last name"]
     },
 
     teacherAddressReg:{
         type:String,
-        required:true
+        required:[true, "please provide your registered address"]
     },
 
     teacherPhone:{
         type:String,
-        required:true
+        required:[true, "please provide your phone number"]
     },
 
     teacherGender:{
         type:String,
-        required:true
+        required:[true, "please provide your gender"],
+        enum:["male","female"]
     },
 
     teacherAge:{
         type:String,
-        required:true
+        required:[true, "please provide your age"]
     },
 
     teacherEmailAddress:{
         type:String,
-        required:true
+        required:[true, "please provide your email"],
+        unique:true,
+        lowecase:true,
+        validate:[validator.isEmail, "Please provide a valid email"]
     },
 
     teacherDuration:{
